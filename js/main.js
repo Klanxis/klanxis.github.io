39c4a0b4fe09ef18a84c9eaddefc3a266d451ad5
Klanxis <klanxis@gmail.com>
2025-04-14 12:52:01 +0100

    I'm committed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+
+//image gallery
 let currentImg = 1;
             function hoverswap(){
                let imgNum = this.id.charAt(3);
                document.querySelector('#large').src="img/kda"+imgNum+".jpg";
                currentImg = imgNum
             }
-
+            //next button loop around
             function next(){
                 if(currentImg == 4){
                     currentImg = 1;
@@ -15,6 +17,7 @@ let currentImg = 1;
                 document.querySelector('#large').src="img/kda"+currentImg+".jpg";
 
             }
+            //prev button loop around
             function prev(){
                 if(currentImg == 1){
                     currentImg = 4;
@@ -25,6 +28,7 @@ let currentImg = 1;
                 document.querySelector('#large').src="img/kda"+currentImg+".jpg";
 
             }
+            //hoverswap listener
             document.querySelector('#img1').addEventListener("mouseover",hoverswap);
             document.querySelector('#img2').addEventListener("mouseover",hoverswap);
             document.querySelector('#img3').addEventListener("mouseover",hoverswap);
